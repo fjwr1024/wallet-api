1e7cdd53543510811a899fc2822fda77d2a124ba
FujiwaraKen <fjwr1024@gmail.com>
2022-11-22 18:26:27 +0900

    example summary data add

--- k6/user-api/tests/healthcheck.js
+++ k6/user-api/tests/healthcheck.js
@@ -1,5 +1,6 @@
 import { check } from 'k6';
 import http from 'k6/http';
+import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export const options = {
   thresholds: {
@@ -17,4 +18,18 @@ export default function () {
   check(res, {
     is_status_200: r => r.status === 200,
   });
+}
+export function handleSummary(data) {
+  console.log('Preparing the end-of-test summary...');
+
+  const resp = http.post('https://httpbin.test.k6.io/anything', JSON.stringify(data));
+  if (resp.status != 200) {
+    console.error('Could not send summary, got status ' + resp.status);
+  }
+
+  return {
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+    '../result/junit.xml': jUnit(data),
+    '../result/result.json': JSON.stringify(data),
+  };
 }
