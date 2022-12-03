db4f5de118ca629f5cbd9d43a47f2e47283a0a93
FujiwaraKen <fjwr1024@gmail.com>
2022-12-04 01:03:52 +0900

    comment fixed

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -19,11 +19,11 @@ export class LoggingService implements LoggerService {
         // アクセスログの出力先設定
         new winstonDailyRotateFile({
           level: 'debug', // debugを指定すると、debug以上のログが出力される
-          datePattern: 'YYYY-MM-DD', // 'YYYY-MM-DD'に設定すると、ログファイルが日付毎に作られる
-          filename: 'application-%DATE%.log', // 保存先ファイル名(上記のdatePatternが含まれる)
-          dirname: 'logs', // ログファイルの保存先ディレクトリ名
+          datePattern: 'YYYY-MM-DD',
+          filename: 'application-%DATE%.log',
+          dirname: 'logs',
           maxSize: '20m', // ローテートするファイルの最大サイズ
-          maxFiles: '30d', // 保存するログの最大数(日数を使う場合は接尾辞として'd'を追加)
+          maxFiles: '30d', // 保存するログの最大日付
         }),
         new winstonDailyRotateFile({
           level: 'error',
