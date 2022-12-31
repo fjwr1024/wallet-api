f6de6c8cc27cc2915755a30bf8a657ba92a648ea
FujiwaraKen <fjwr1024@gmail.com>
2022-12-31 12:39:26 +0900

    role guard add

---
+++ src/entities/news.entity.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'news' })
+export class News {
+  @PrimaryGeneratedColumn('increment', { name: 'id' })
+  id: number;
+
+  @Column({ name: 'title', unique: false, nullable: false })
+  title: string;
+
+  @Column({ name: 'title', unique: false, nullable: false })
+  body: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updatedAt!: Date;
+}
