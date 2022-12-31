03aa577f8d8f1eb2c84a7758da4163ad0c0bffd0
FujiwaraKen <fjwr1024@gmail.com>
2023-01-01 01:55:58 +0900

    get news list api add

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { News } from 'src/entities/news.entity';
+import { NewsService } from './news.service';
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {}
+
+  // react admin get list用apiK
+  @Get()
+  async getNews(): Promise<News[]> {
+    const res = await this.newsService.getNews();
+    return res;
+  }
+}
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { AppDataSource } from 'src/data-source';
+import { News } from 'src/entities/news.entity';
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+  async getNews(): Promise<News[]> {
+    const res = await AppDataSource.manager.find(News);
+    return res;
+  }
+}
