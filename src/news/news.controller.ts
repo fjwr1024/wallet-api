5a7f6d5e77c509827edac3f2978c8a4de9a677ae
FujiwaraKen <fjwr1024@gmail.com>
2023-01-01 02:18:34 +0900

    news info add

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { News } from 'src/entities/news.entity';
 import { NewsService } from './news.service';
 
@@ -6,10 +6,15 @@ import { NewsService } from './news.service';
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
-  // react admin get list用apiK
   @Get()
   async getNews(): Promise<News[]> {
     const res = await this.newsService.getNews();
+    return res;
+  }
+
+  @Get('/:id')
+  async getNewsInfo(@Param('id', ParseIntPipe) id: number): Promise<News> {
+    const res = await this.newsService.getNewsInfo(id);
     return res;
   }
 }
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AppDataSource } from 'src/data-source';
 import { News } from 'src/entities/news.entity';
 
@@ -6,6 +6,17 @@ import { News } from 'src/entities/news.entity';
 export class NewsService {
   async getNews(): Promise<News[]> {
     const res = await AppDataSource.manager.find(News);
+    return res;
+  }
+
+  public async getNewsInfo(id): Promise<News> {
+    const res = await AppDataSource.manager.findOneBy(News, {
+      id,
+    });
+
+    if (!res) {
+      throw new NotFoundException('This id News is not found');
+    }
     return res;
   }
 }
