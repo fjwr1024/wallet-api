b38ef12c62442e25effea7c701e94abcf93b87b2
FujiwaraKen <fjwr1024@gmail.com>
2022-12-31 12:45:02 +0900

    role fix

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,18 +1,21 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Role } from 'src/auth/role.enum';
+import { ROLES_KEY } from 'src/decorator/role.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  canActivate(ctx: ExecutionContext): boolean {
-    const requiredStatuses = this.reflector.get<string[]>('statuses', ctx.getHandler());
-
-    if (!requiredStatuses) {
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
       return true;
     }
-
-    const { user } = ctx.switchToHttp().getRequest();
-    return requiredStatuses.some(status => user.status.includes(status));
+    const { user } = context.switchToHttp().getRequest();
+    return requiredRoles.some(role => user.roles?.includes(role));
   }
 }
