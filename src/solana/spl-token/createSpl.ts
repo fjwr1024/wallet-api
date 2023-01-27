0d35ae18102792bccb67348645fe3d049a4daa59
FujiwaraKen <fjwr1024@gmail.com>
2023-01-28 01:38:33 +0900

    token metadata inspltoken add

--- src/solana/spl-token/createSpl.ts
+++ src/solana/spl-token/createSpl.ts
@@ -1,14 +1,25 @@
 import { SplToken, Pubkey } from '@solana-suite/core';
+import { StorageType } from '@solana-suite/shared-metaplex';
 
 export const createSplToken = async (totalAmount, decimals, ownerWalletAddress, ownerSecretKey) => {
   console.log(totalAmount);
   console.log(decimals);
 
+  const tokenMetadata = {
+    name: 'solana-suite-token',
+    symbol: 'SST',
+    royalty: 50,
+    filePath: '../../uploads/0aea4dae92de4cc1b3d7ead95e1b1f52',
+    storageType: 'nftStorage' as StorageType,
+    isMutable: false,
+  };
+
   const inst1 = await SplToken.mint(
     ownerWalletAddress.toPublicKey(),
     ownerSecretKey.toKeypair(),
     totalAmount,
-    decimals
+    decimals,
+    tokenMetadata
   );
 
   const mint = inst1.unwrap().data as Pubkey;
