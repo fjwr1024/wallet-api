8b4a683c2857a72df7324862176c5c21d934ad9b
FujiwaraKen <fjwr1024@gmail.com>
2023-02-07 04:15:14 +0900

    update stripe logic

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,13 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppDataSource } from 'src/data-source';
 import { Orders } from 'src/entities/orders.entity';
 import { User } from 'src/entities/user.entity';
+import Stripe from 'stripe';
 import { OrdersDto } from './dto/orders.dto';
 
 @Injectable()
 export class OrdersService {
+  private stripe: Stripe;
+
+  constructor(private readonly config: ConfigService) {
+    this.stripe = new Stripe(this.config.get('STRIPE_SECRET_KEY'), {
+      apiVersion: '2022-11-15',
+      typescript: true,
+    });
+  }
+
   async order(ordersDto: OrdersDto): Promise<string> {
-    console.log('ordersDto', ordersDto);
     const id = ordersDto.userId;
 
     const res = await AppDataSource.manager.findOneBy(User, {
@@ -17,6 +27,14 @@ export class OrdersService {
     if (!res) {
       throw new NotFoundException('User is not found');
     }
+
+    const charge = await this.stripe.charges.create({
+      amount: ordersDto.amount,
+      currency: 'usd',
+      description: `Order ${new Date()} by ${ordersDto.userId}`,
+    });
+
+    console.log('charge', charge);
 
     const orders = new Orders();
     orders.userId = ordersDto.userId;
