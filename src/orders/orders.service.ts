5314a1b8176cf7afc3126f5a8defdce3b9b5bbe3
FujiwaraKen <fjwr1024@gmail.com>
2023-02-07 03:31:19 +0900

    order error handling

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,12 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AppDataSource } from 'src/data-source';
 import { Orders } from 'src/entities/orders.entity';
+import { User } from 'src/entities/user.entity';
 import { OrdersDto } from './dto/orders.dto';
 
 @Injectable()
 export class OrdersService {
   async order(ordersDto: OrdersDto): Promise<string> {
     console.log('ordersDto', ordersDto);
+    const id = ordersDto.userId;
+
+    const res = await AppDataSource.manager.findOneBy(User, {
+      id,
+    });
+
+    if (!res) {
+      throw new NotFoundException('User is not found');
+    }
 
     const orders = new Orders();
     orders.userId = ordersDto.userId;
