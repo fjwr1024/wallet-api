316a6fcb6bb010d978cc11fcd6a7380a4b3e87b3
FujiwaraKen <fjwr1024@gmail.com>
2023-03-03 03:29:43 +0900

    add mail title

--- src/utils/mail/mail-content.ts
+++ src/utils/mail/mail-content.ts
@@ -1,7 +1,11 @@
+export const AUTH_MAIL_TITLE = `
+    [WalletAPI]Check your verification code
+`;
+
 export const AUTH_MAIL_BODY = `
   Your Authorization code is. 
   Please enter the verification code to complete your membership registration.
   If you do not recognize the email, please discard the email as is. 
   This email address is for sending only. 
   I have not received a reply.
-  `;
+`;
