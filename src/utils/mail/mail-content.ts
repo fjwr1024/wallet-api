d9abac07157c4a9ba5d6b48f98fe512b37def9db
FujiwaraKen <fjwr1024@gmail.com>
2023-03-03 03:33:02 +0900

    add verify code var

--- src/utils/mail/mail-content.ts
+++ src/utils/mail/mail-content.ts
@@ -2,10 +2,13 @@ export const AUTH_MAIL_TITLE = `
     [WalletAPI]Check your verification code
 `;
 
-export const AUTH_MAIL_BODY = `
-  Your Authorization code is. 
+export const AUTH_MAIL_BODY = (verifyCode: string) => {
+  const body = `
+  Your Authorization code is ${verifyCode}. 
   Please enter the verification code to complete your membership registration.
   If you do not recognize the email, please discard the email as is. 
   This email address is for sending only. 
   I have not received a reply.
 `;
+  return body;
+};
