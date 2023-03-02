ed9b62b63c1a087a015ab64fe7b2b6a4594030d4
FujiwaraKen <fjwr1024@gmail.com>
2023-03-03 03:28:25 +0900

    add mail content

---
+++ src/utils/mail/mail-content.ts
@@ -0,0 +1,7 @@
+export const AUTH_MAIL_BODY = `
+  Your Authorization code is. 
+  Please enter the verification code to complete your membership registration.
+  If you do not recognize the email, please discard the email as is. 
+  This email address is for sending only. 
+  I have not received a reply.
+  `;
