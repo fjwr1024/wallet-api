35f448140f3fe56f171d3a4da3a163d53d6edbe0
FujiwaraKen <fjwr1024@gmail.com>
2023-03-05 02:06:17 +0900

    mv directry add migration sh

--- scripts/migration.sh
+++ scripts/migration.sh
@@ -1,2 +1,20 @@
-yarn migration:generate
-yarn migration:run
+#!/bin/bash
+set -eux
+
+# yarn migration:generate
+# yarn migration:run
+
+SCRIPT_DIR=$(
+    cd $(dirname $0)
+    pwd
+)
+
+DIR_PATH="$SCRIPT_DIR/../src/migration/"
+
+# directry exists confirm
+if [ -d "$dir_path" ]; then
+    rm -r $DIR_PATH/*
+    echo "Directory contents have been deleted successfully."
+else
+    echo "Directory does not exist."
+fi
