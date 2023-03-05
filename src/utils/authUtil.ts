602c942cb234c7de7d0c57e11569706b8fedbc88
FujiwaraKen <fjwr1024@gmail.com>
2023-03-06 02:01:06 +0900

    add auth util

---
+++ src/utils/authUtil.ts
@@ -0,0 +1,16 @@
+import { HttpStatus, UnauthorizedException } from '@nestjs/common';
+import { AppDataSource } from 'src/data-source';
+import { AuthEmail } from 'src/entities/auth-email.entity';
+
+export const confirmSmsCode = async ({ id, authCode }): Promise<number> => {
+  const getAuthCodeId = await AppDataSource.manager.findOneBy(AuthEmail, {
+    id,
+  });
+  const now = new Date();
+
+  if (authCode === getAuthCodeId.sentCode && now < getAuthCodeId.limitTime) {
+    return HttpStatus.OK;
+  } else {
+    throw new UnauthorizedException('Verfication incorrect or time over');
+  }
+};
