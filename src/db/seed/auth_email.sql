8bf934fabcf752cc4f3f4dacb2a72d1544f9a328
FujiwaraKen <fjwr1024@gmail.com>
2023-03-06 03:44:33 +0900

    seed sql add

---
+++ src/db/seed/auth_email.sql
@@ -0,0 +1,2 @@
+INSERT INTO wallet_db.auth_email (email,sent_code,sent_at,limit_time,created_at,updated_at) VALUES
+	 ('sample24@email.com',711164,1,'2023-03-05 16:17:04','2023-03-05 15:17:04.383546','2023-03-05 15:17:04.383546');
---
+++ src/db/seed/news_insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO wallet_db.news (created_at,updated_at,title,body,is_published) VALUES
+	 ('2023-02-11 05:10:14.379197','2023-02-11 17:02:29.347852','Lorem ipsum dolor sit amet','Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 17:02:29.352350','Lorem ipsum dolor sit amet','Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 17:02:29.354875','Lorem ipsum dolor sit amet','Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',1),
+	 ('2023-02-11 05:10:14.379197','2023-03-06 02:58:20','title7','body7',0),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1);
+INSERT INTO wallet_db.news (created_at,updated_at,title,body,is_published) VALUES
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1),
+	 ('2023-02-11 05:10:14.379197','2023-02-11 05:10:14.379197','title','body',1);
