219cfef3e5eacb4e2a9aae59c8c09b0206cf5b52
FujiwaraKen <fjwr1024@gmail.com>
2023-09-22 14:51:43 +0900

    nft storage changed

--- scripts/nftStorageChange.sh
+++ scripts/nftStorageChange.sh
@@ -11,4 +11,4 @@ cd $SOLANA_DIR
 echo ${SOLANA_DIR}
 
 # command
-./solana-suite-config.js -n eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDYzNzQ0MDRkOUEwYWQyRUNEQjAyNTFCMWY4MjYyMzFENDdCNmVjNjIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5Mjk0OTIwODIzMCwibmFtZSI6IndhbGxldF9hcGkifQ._JItDrNriRqiCGWA86nptTR0xBfGXUpyQA99NUS9Tjs
+./solana-suite-config.js -n
--- src/solana/spl-token/createMemo.ts
+++ src/solana/spl-token/createMemo.ts
@@ -4,12 +4,17 @@ import { Node } from '@solana-suite/shared';
 export const createMemo = async (splToken: any, comment: string, walletAddress: string, secretKey: string) => {
   const memo = Memo.create(comment, walletAddress, secretKey);
 
+  console.log('splToken', splToken);
+  console.log('# memo: ', memo);
+  console.log('memo', walletAddress);
+  console.log('memo', secretKey);
+
   (await [splToken, memo].submit()).match(
     async value => {
       console.log('# memo sig: ', value.toExplorerUrl());
       await Node.confirmedSig(value);
     },
-    error => console.error('error', error)
+    error => console.log('error', error)
   );
 
   return 'ok';
