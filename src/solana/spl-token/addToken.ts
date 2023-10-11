514947ca7757456d6b663cdd5b85835a1335933c
FujiwaraKen <fjwr1024@gmail.com>
2023-10-11 17:19:43 +0900

    add tokenm

---
+++ src/solana/spl-token/addToken.ts
@@ -0,0 +1,8 @@
+import { SplToken } from '@solana-suite/core';
+
+export const addToken = async (tokenAddress, owner, secretKey, totalAmount, mintDecimal) => {
+  const result = await SplToken.add(tokenAddress, owner, secretKey, totalAmount, mintDecimal);
+
+  return result;
+  t;
+};
