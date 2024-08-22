a83feea031c7004b7a746d5cda376f6897b2b950
FujiwaraKen <fjwr1024@gmail.com>
2024-08-22 11:41:23 +0900

    submit hex updated

--- src/solana/spl-token/submitHex.ts
+++ src/solana/spl-token/submitHex.ts
@@ -1,16 +1,16 @@
-// import assert from 'assert';
+import assert from 'assert';
 
-// import { PartialSignInstruction } from '@solana-suite/multisig';
+import { Result } from '@solana-suite/utils';
 
-// export const submitHex = async (hex: string, ownerSecretKey: string) => {
-//   console.log(ownerSecretKey);
-//   console.log(hex);
+export const submitHex = async (hex: any, ownerSecretKey: string) => {
+  console.log(ownerSecretKey);
+  console.log(hex);
 
-//   const obj = new PartialSignInstruction(hex);
-//   const res = await obj.submit(ownerSecretKey);
-//   res.match(
-//     ok => console.log('# tx signature: ', ok),
-//     err => assert.fail(err.message)
-//   );
-//   return res;
-// };
+  const obj = Result.ok(hex.unwrap().hexInstruction);
+  const res = await obj.submit({ feePayer: ownerSecretKey });
+  res.match(
+    ok => console.log('# tx signature: ', ok),
+    err => assert.fail(err.message)
+  );
+  return res;
+};
